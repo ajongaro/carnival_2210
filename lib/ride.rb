a833793b647036862d102eed53cdde0e3a2b424d
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-11-14 09:28:36 -0700

    feat: Add initial ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,10 @@
 class Ride
+  attr_reader :name, :min_height, :admission_fee, :excitement
 
+  def initialize(attributes)
+    @name = attributes[:name]
+    @min_height = attributes[:min_height]
+    @admission_fee = attributes[:admission_fee]
+    @excitement = attributes[:excitement]
+  end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -2,7 +2,7 @@ require './lib/visitor'
 require './lib/ride'
 
 RSpec.describe Ride do
-  let(ride1) { Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle }) }
+  let(:ride1) { Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle }) }
   let(:visitor1) { Visitor.new('Bruce', 54, '$10')}
   let(:visitor2) { Visitor.new('Tucker', 36, '$5')}
   let(:visitor3) { Visitor.new('Penny', 64, '$15')}
